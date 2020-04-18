c1e7e967e390c2b1b4c8d15a805d43481265207b
sid.ni <sid.ni@ef.com>
2020-04-18 11:13:43 +0800

    #add

---
+++ src/test/java/com/niyongcheng/demo/SpringEurekaApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.niyongcheng.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/

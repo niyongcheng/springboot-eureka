e1b30cbf6668fdac55cfa31aa966279e97a37abc
sid.ni <sid.ni@ef.com>
2020-04-20 15:45:49 +0800

    #add

--- src/main/java/com/niyongcheng/demo/SpringEurekaApplication.java
+++ src/main/java/com/niyongcheng/demo/SpringEurekaApplication.java
@@ -3,9 +3,11 @@ package com.niyongcheng.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -19,7 +21,21 @@ public class SpringEurekaApplication {
     static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
+            http.csrf()
+                    .disable()
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic();
+            ;
+        }
+
+        @Override
+        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                    .withUser("admin").password("admin")
+                    .authorities("ADMIN");
         }
     }
 }
